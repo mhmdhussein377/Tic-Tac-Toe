1819bae1534482c962845480611854205fa56bf6
Mohammad Hussein <mohammad.hussein377@gmail.com>
2023-07-13 10:49:22 +0300

    checking for the winner

--- script/script.js
+++ script/script.js
@@ -2,11 +2,50 @@ let cells = document.querySelectorAll(".cell");
 cells = Array.from(cells);
 
 let currentPlayer = "X";
+let winner = "";
+let winConditions = [
+    [
+        0, 1, 2
+    ],
+    [
+        3, 4, 5
+    ],
+    [
+        6, 7, 8
+    ],
+    [
+        0, 3, 6
+    ],
+    [
+        1, 4, 7
+    ],
+    [
+        2, 5, 8
+    ],
+    [
+        0, 4, 8
+    ],
+    [2, 4, 6]
+];
 
-cells.forEach(function(cell) {
-    cell.addEventListener('click', function() {
-        if(cell.innerText.trim() != "") return;
-        cell.innerText = currentPlayer;
-        currentPlayer = currentPlayer == "X" ? "O" : "X";
+function checkWinner() {
+    for (let i = 0; i < winConditions.length; i++) {
+        if (cells[winConditions[i][0]].innerText != "" && cells[winConditions[i][0]].innerText === cells[winConditions[i][1]].innerText && cells[winConditions[i][1]].innerText === cells[winConditions[i][2]].innerText) {
+            winner = currentPlayer;
+        }
+    }
+};
+
+cells
+    .forEach(function (cell) {
+        cell
+            .addEventListener('click', function () {
+                if (cell.innerText.trim() != "") 
+                    return;
+                cell.innerText = currentPlayer;
+                checkWinner();
+                currentPlayer = currentPlayer == "X"
+                    ? "O"
+                    : "X";
+            })
     })
-})
