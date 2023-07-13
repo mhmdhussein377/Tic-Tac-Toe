bdf0cc176e2a175fe27d0f6a729917998a7d51bc
Mohammad Hussein <mohammad.hussein377@gmail.com>
2023-07-13 13:04:21 +0300

    improving the winner checking feature(Draw)

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 let board = document.querySelector('.board');
 let cells = document.querySelectorAll(".cell");
-let resetGame = document.querySelector("#resetGame");
+let resetGameBtn = document.querySelector("#resetGame");
 let resetScore = document.querySelector("#resetScore");
 let winnerDiv = document.querySelector(".winner");
 let playerxScoreSpan = document.querySelector(".xScore");
@@ -40,6 +40,9 @@ playerxScoreSpan.innerText = playerxScore;
 playeroScoreSpan.innerText = playeroScore;
 
 function checkWinner() {
+
+    let isDraw = true;
+
     for (let i = 0; i < winConditions.length; i++) {
         if (cells[winConditions[i][0]].innerText != "" && cells[winConditions[i][0]].innerText === cells[winConditions[i][1]].innerText && cells[winConditions[i][1]].innerText === cells[winConditions[i][2]].innerText) {
             winner = currentPlayer;
@@ -47,10 +50,24 @@ function checkWinner() {
             ]);
             winnerDiv.innerText = `Player ${currentPlayer} Won`
 
-            currentPlayer == "X" ? playerxScore += 1 : playeroScore += 1;
+            currentPlayer == "X"
+                ? playerxScore += 1
+                : playeroScore += 1;
             playerxScoreSpan.innerText = playerxScore;
             playeroScoreSpan.innerText = playeroScore;
+            return;
         }
+    }
+
+    for (let i = 0; i < cells.length; i++) {
+        if (cells[i].innerText === "") {
+            isDraw = false;
+            break;
+        }
+    }
+
+    if (isDraw) {
+        winnerDiv.innerText = "It's a Draw"
     }
 };
 
@@ -62,9 +79,8 @@ function highlightCells(combination) {
     }
 }
 
-resetGame
-    .addEventListener("click", function () {
-        board.innerHTML = `<div class="cell"></div>
+function resetGame() {
+    board.innerHTML = `<div class="cell"></div>
         <div class="cell"></div>
         <div class="cell"></div>
         <div class="cell"></div>
@@ -74,16 +90,23 @@ resetGame
         <div class="cell"></div>
         <div class="cell"></div>`;
 
-        cells = document.querySelectorAll(".cell");
-        cells = Array.from(cells);
-        addClickEvent();
-        winner = "";
-        winnerDiv.innerText = "";
-    });
+    cells = document.querySelectorAll(".cell");
+    cells = Array.from(cells);
+    addClickEvent();
+    winner = "";
+    winnerDiv.innerText = "";
+}
 
-resetScore.addEventListener("click", function() {
+resetGameBtn.addEventListener("click", resetGame);
+
+resetScore.addEventListener("click", function () {
     playerxScore = 0;
     playeroScore = 0;
+
+    playerxScoreSpan.innerText = playerxScore;
+    playeroScoreSpan.innerText = playeroScore;
+
+    resetGame()
 })
 
 function addClickEvent() {
@@ -91,7 +114,9 @@ function addClickEvent() {
         .forEach(function (cell) {
             cell
                 .addEventListener("click", function () {
-                    if (cell.innerText.trim() != "") 
+                    if (winner !== "") 
+                        return
+                    if (cell.innerText != "") 
                         return;
                     cell.innerText = currentPlayer;
                     checkWinner();
